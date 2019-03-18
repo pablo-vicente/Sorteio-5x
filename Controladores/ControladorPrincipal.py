6da4e7c0125590753ed8b656a42d2b19562929d2
Pablo Vicente <pablo.vicente@grad.ufsc.br>
2019-03-18 13:47:52 -0300

    Metodo vericar_arquivo(): valida se todos campos estão preenchidos,

--- Controladores/ControladorPrincipal.py
+++ Controladores/ControladorPrincipal.py
@@ -1,27 +1,42 @@
 class ControladorPrincipal:
+    tabela_participantes = ['Id', 'Nome Completo', 'Quantidade de Cupons']
+    def __init__(self):
 
-    def __init__(self, tabela_participantes = ['Id', 'Nome Completo', 'Quantidade de Cupons']):
         pass
 
-    def gerarCupons(self):
-
-
-        file = open('Gerar cupons.csv', 'r')
+    def validar_arquivo(self):
+        file = open('Base.csv', "r", encoding="ISO-8859-1")
         reader = file.readline()  # leitura do cabeçalho
-        contadorDeLinhas = 1  # quantidades de linhas lidas do arquivo
-        errosEncontrados = ''  # Inicia variável de erros
+        self.limpar_arquivo_erros()
+        self.log_erros(reader.replace('\n', ';Erros Encontrados\n'))  # Cria o cabeçaçho do arquivo de erros
+        erros_encontrados = ''  # Inicia variável de erros
         for line in file:
-            contadorDeLinhas += +1  # Adiciona uma linha ao contador
-            temp = line.split(';')  # Leitura de cada linha
+            temp = line.replace('\n','').split(';')  # Divide a Linha em partes
             if len(temp) != 3:  # verifica se possui 3 colunas
-                errosEncontrados = errosEncontrados + "LINHA " + str(contadorDeLinhas) + ": Possui mais de 4 (quatro) colunas. Verifique se as colunas ao lado estão em branco. \n"
-            if temp[0] is "":  #Verifica se campo ID está em branco
-                errosEncontrados = errosEncontrados + "LINHA " + str(contadorDeLinhas) + ": Campo " + tabela_participantes + " está em branco.\n"
+                erros_encontrados = 'Linha possui mais que 3 campos.'
+            posicao = 0  # Incia na primeira posição ID
+            for titulo_campo in temp:  # varrer cs campos da linha
+                if titulo_campo is '':  # verifica se cada campos em branco
+                    campo = self.tabela_participantes[posicao]  # Pega o nome do campo que está em branco
+                    # Verfica se já possui alguma mensagem de erro anterior
+                    if erros_encontrados is '':
+                        erros_encontrados = '{} está em branco.'.format(campo)  # Tem as três colunas e algum campo em branco
+                    else:
+                        erros_encontrados = '{},{} está em branco'.format(erros_encontrados, campo)
+                posicao += 1
+            posicao =0
+            erro = '{}{}{}'.format(line.replace('\n', ';'), erros_encontrados, '\n')
+            erros_encontrados = ''  # Limpa erros da linha anterior
+            self.log_erros(erro)
 
-        return errosEncontrados
+    def log_erros(self, erros_encontrados):
+        arquivo_erros = open('Erros.csv', "a", encoding="utf8")
+        arquivo_erros.write(erros_encontrados)
+        arquivo_erros.close()
 
-    def __mensage_erro(self, erros_encontrados, contador_linhas, campo):
-        erros_encontrados = '{} Linha {}: Campo {} está em branco.\n'.format(erros_encontrados,str(contador_linhas), '')
+    def limpar_arquivo_erros(self):
+        arquivo_erros = open('Erros.csv', "w")
+        arquivo_erros.close()
 
-
-
+    def gerar_cupons(self):
+        pass
--- Main.py
+++ Main.py
@@ -1,4 +1,13 @@
 from Controladores.ControladorPrincipal import ControladorPrincipal
 
 controladorPrincipal = ControladorPrincipal()
-print(controladorPrincipal.gerarCupons())
+controladorPrincipal.validar_arquivo()
+
+
+#  file = open('Gerar cupons.csv', 'r')
+#  reader = file.readline()  # leitura do cabeçalho
+#  for line in file:
+#      temp = line.split(';')  # Divide a linha em partes, verifica se tem somente 3 campos
+#      for titulo_campo in temp:  # varrer cs campos da linha
+#          n = temp.index(titulo_campo)
+#          print(titulo_campo, n)
